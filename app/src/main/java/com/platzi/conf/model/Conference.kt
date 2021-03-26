6fafc7c40b8b07351db7d1671f9ab83d669cc823
Trilador123 <fabrix_fvz@hotmail.com>
2021-03-25 18:27:42 -0600

    proyecto version 1

---
+++ app/src/main/java/com/platzi/conf/model/Conference.kt
@@ -0,0 +1,15 @@
+package com.platzi.conf.model
+
+import java.io.Serializable
+import java.util.*
+
+class Conference: Serializable {
+    lateinit var speaker: String
+    lateinit var title: String
+    lateinit var description: String
+    lateinit var tag: String
+    lateinit var datetime: Date
+
+
+
+}
---
+++ app/src/main/java/com/platzi/conf/model/Ubication.kt
@@ -0,0 +1,12 @@
+package com.platzi.conf.model
+
+class Ubication {
+    val name = "Centro Cultural Roberto Cantoral"
+    val address = "Puente Xoco s/n, Xoco, 03330 Ciudad de Mexico, CDMX"
+    val latitude = 19.362028
+    val longitude = -99.166414
+    val phone = "+52 55 1519 4670"
+    val website = "https://elcantoral.com/"
+    val photo = "httos://lh5.googleusercontent.com/p/AF1QipPJ-rZ_uZmMzvQT3iVlA832hei26T_qjFbJYiW6=w408-h270-k-no"
+
+}
